5d73ce925a1347f7e84a403e104abce10eca6c8c
Toxic Arjun <125901796+toxicworld75@users.noreply.github.com>
2024-02-16 13:37:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ START_IMG = getenv("START_IMG", "https://te.legra.ph/file/b3d0e737737d67a5bf5a5.
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/sastatony")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/team_ligit")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/privatearjun")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/privatearjun")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
